e7940dc64d61a67b74b88be8b6046a1acbbf61f7
pierbin24 <63203868+pierbin24@users.noreply.github.com>
2020-04-07 12:11:33 +0200

    Update esercizio-C-2020-04-07.c

--- src/esercizio-C-2020-04-07.c
+++ src/esercizio-C-2020-04-07.c
@@ -1,8 +1,67 @@
-/*
- * esercizio-C-2020-04-07.c
- *
- *  Created on: Apr 2, 2020
- *      Author: marco
- */
+//in alcune esecuzioni mi entra in loop e non porta a terime il programma
+//ho provato a individuare il problema anche con il debug ma non ci riesco
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define MAX 100
+//creo un array di numeri in ordine causale di tanti numeri quanti sono le parole
+int *array_ordine_casuale ( int parole){
+
+	int *result = calloc(parole, sizeof(int));
+
+	 if(result == NULL){
+	        printf("\ncalloc error!\n");
+	        exit(2);
+	    }
+
+	int r;
+
+	for(int i = 0; i<parole; i++){
+		r=(rand()%parole);
+		result[i] = r;
+
+		//controllo che il numero non sia già presente all'interno dell'array
+		for (int j=0; j < parole; j++){
+			if (result[i]==result[j]){
+				i--;
+				break;
+			}
+		}
+	}
+
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+
+	char stringa[] = "hello world mi chiamo pino sono date delle stringhe";
+	char s[] = " ";
+	char *token;
+	printf("%s\n", stringa);
+
+	token = strtok(stringa, s);
+
+	int parole = 0;
+	char *lista[MAX];
+	lista[0] = token;
+	while (token != NULL ) {
+
+			token = strtok(NULL, s);
+			parole++;
+			lista[parole]=token;
+	   }
+
+	//printf("Le parole all'interno della stringa sono: %d\n", parole);
+	int *casuale = array_ordine_casuale(parole);
+	for(int i = 0; i<parole; i++){
+		int p = casuale[i];
+		printf("%s ", lista[p]);
+	}
+}
 
 
